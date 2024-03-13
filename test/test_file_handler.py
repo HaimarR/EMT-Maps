8cf7f278ccb7340305f48271601604e319bbcab3
Haimar <haimarrubio24@gmail.com>
2024-03-13 13:49:41 -0400

    better tests

--- test/test_file_handler.py
+++ test/test_file_handler.py
@@ -3,9 +3,15 @@ import unittest
 from unittest.mock import patch, MagicMock
 from src.file_handler import FileHandler
 
+SAVE_FOLDER = "local_maps/"
+PLACE_NAME = "San Sebastian, Guipuzcoa, Spain"
+FILENAME = "San_Sebastian-Guipuzcoa-Spain"
+FILE_EXT = ".graphml"
+FULL_PATH = f"{SAVE_FOLDER}{FILENAME}{FILE_EXT}"
+
 class TestFileHandler(unittest.TestCase):
     def setUp(self):
-        self.place_name = "San Sebastian, Guipuzcoa, Spain"
+        self.place_name = PLACE_NAME
         self.file_handler = FileHandler(self.place_name)
 
     def tearDown(self):
@@ -15,12 +21,10 @@ class TestFileHandler(unittest.TestCase):
         self.assertEqual(self.file_handler.getSaveFolder(), "local_maps/")
 
     def test_get_filename(self):
-        expected_filename = "San_Sebastian-Guipuzcoa-Spain.graphml"
-        self.assertEqual(self.file_handler.getFilename(), expected_filename)
+        self.assertEqual(self.file_handler.getFilename(), f"{FILENAME}{FILE_EXT}")
 
     def test_get_full_path(self):
-        expected_full_path = "local_maps/San_Sebastian-Guipuzcoa-Spain.graphml"
-        self.assertEqual(self.file_handler.getFullPath(), expected_full_path)
+        self.assertEqual(self.file_handler.getFullPath(), FULL_PATH)
 
     @patch('src.file_handler.ox.save_graphml')
     def test_save_graph_to_file(self, mock_save_graphml):
